577b904958044b3562dd33c16e3288d5523fc0f0
Marcin Magnus <mag_dex@o2.pl>
2021-07-31 14:28:42 +0200

    pdf_TemplateScript.sh dealing with multiple files

--- pdf_TemplateScript.sh
+++ pdf_TemplateScript.sh
@@ -1,2 +1,17 @@
 ebook-convert Pocket.recipe .pdf #& open Pocket.mobi
-mv -v Pocket.pdf "/Users/magnus/Dropbox/boox/Pocket `date +'%b %d %Y'`.pdf"
+#ebook-convert Pocket.recipe .mobi #& open Pocket.mobi
+
+# this will overwrite a file there ! add something to the name if you want to prevent that
+mv  -v Pocket.pdf "/Users/magnus/Dropbox/boox/Pocket `date +'%b %d %Y %H%M'`.pdf"
+# --backup=numbered --suffix '.pdf'# go to a folder and clean up naming
+#cd /Users/magnus/Dropbox/boox
+#mmv -v "*.*~*~" "#1_#3.#2"
+#cd -
+
+# credits <https://serverfault.com/questions/267255/mv-rename-if-exists>
+#                     "*.*~*~" "#1_#3.#2"
+# Pocket Mar 08 2021.pdf.~1~ -> Pocket Mar 08 2021_1.pdf. : done
+# Pocket Mar 08 2021.pdf.~2~ -> Pocket Mar 08 2021_2.pdf. : done
+
+# sync
+boox.py
