506fdb1543981fc3b7aa358e3afbe5c660c95ae6
Ramiro Morales <cramm0@gmail.com>
2023-12-23 23:32:20 +0100

    Run black over Python files

--- push/push-script.py
+++ push/push-script.py
@@ -8,27 +8,29 @@ import glob
 import os
 import shutil
 
+
 def get_parser():
     parser = argparse.ArgumentParser(
-        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
 
-    #parser.add_argument('-', "--", help="", default="")
-    parser.add_argument("-d", "--dev",
-                        action="store_true", help="don't remove files")
-    parser.add_argument("-v", "--verbose",
-                        action="store_true", help="be verbose")
-    parser.add_argument("path", help="", default="") # nargs='+')
-    parser.add_argument("script", help="", default="") # nargs='+')
+    # parser.add_argument('-', "--", help="", default="")
+    parser.add_argument("-d", "--dev", action="store_true", help="don't remove files")
+    parser.add_argument("-v", "--verbose", action="store_true", help="be verbose")
+    parser.add_argument("path", help="", default="")  # nargs='+')
+    parser.add_argument("script", help="", default="")  # nargs='+')
     return parser
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
-    path = args.path + '/*'
+    path = args.path + "/*"
     if len(glob.glob(path)):
-        os.system("osascript -e 'display notification \"Push Pocket\" with title \"Pocket+\"'")
-        os.system('source ' + args.script)
+        os.system(
+            'osascript -e \'display notification "Push Pocket" with title "Pocket+"\''
+        )
+        os.system("source " + args.script)
     if not args.dev:
         # fetch file list again to remove all files there
         for f in glob.glob(path):
